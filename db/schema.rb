18f98cf60a59e131fec8592afb12513d82a291a3
BitBear <bitbear.io@gmail.com>
2022-02-09 16:40:47 +0900

    :memo: add 9.1.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_023713) do
+ActiveRecord::Schema.define(version: 2022_02_09_072301) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_023713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
