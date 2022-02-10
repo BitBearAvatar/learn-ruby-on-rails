a44fc6d3f1ca6e23c5fd2366ed484cc4dc5726f9
BitBear <bitbear.io@gmail.com>
2022-02-10 16:06:41 +0900

    :memo: add 13.1.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_10_061931) do
+ActiveRecord::Schema.define(version: 2022_02_10_063612) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_02_10_061931) do
     t.datetime "reset_sent_at"
   end
 
+  add_foreign_key "microposts", "users"
 end
